86dcf99ce9d3d14b6be1104565511ed9a9630a03
alhhITU <alhh@itu.dk>
2026-01-02 15:27:48 +0100

    Files containing frontend, made static, and simple connection between fastapi and html has been made

--- App.py
+++ App.py
@@ -1,2 +1,15 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+import uvicorn
 
+app = FastAPI()
+app.mount("/FrontEnd", StaticFiles(directory="FrontEnd"), name="FrontEnd")
+
+@app.get('/')
+def index():
+    path = "./FrontEnd/Pages/FrontPage.html"
+    return FileResponse(path)
+
+
+# uvicorn App:app --reload
